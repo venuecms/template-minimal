8915119b109cdebdb9a73509bb60a3d3e4f0da24
Scott Cazan <scottcazan@gmail.com>
2024-12-29 15:07:17 +0100

    add contentJSON to page

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,7 +1,10 @@
 import { type Page as VenuePage, getLocalizedContent } from "@venuecms/sdk";
 import { useLocale } from "next-intl";
 
+import { ContentRender, RenderNode } from "@/lib/utils/renderer";
+
 import { ColumnLeft, ColumnRight, TwoColumnLayout } from "../layout";
+import { renderedStyles } from "../utils";
 
 export const Page = ({ page }: { page: VenuePage }) => {
   const locale = useLocale();
@@ -19,7 +22,11 @@ export const Page = ({ page }: { page: VenuePage }) => {
       </ColumnLeft>
 
       <ColumnRight>
-        <p className="max-w-[42rem] text-sm">{content.content}</p>
+        <div className="max-w-[42rem] text-sm">
+          {(content.contentJSON?.content as Array<RenderNode>).map((node) => (
+            <ContentRender classes={renderedStyles} node={node} />
+          ))}
+        </div>
       </ColumnRight>
     </TwoColumnLayout>
   );

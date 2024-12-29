6602d2aca2fcae4cf3fa30dab7ede419b0d83baa
Scott Cazan <scottcazan@gmail.com>
2024-12-29 17:27:15 +0100

    add archive page

---
+++ src/app/[locale]/archive/page.tsx
@@ -0,0 +1,27 @@
+import { getEvents, getSite } from "@venuecms/sdk";
+
+import { EventsList } from "@/components/EventList";
+import { ColumnLeft, ColumnRight, TwoColumnLayout } from "@/components/layout";
+
+const Home = async () => {
+  const [{ data: events }] = await Promise.all([getEvents({ limit: 60 })]);
+
+  return (
+    <TwoColumnLayout>
+      <ColumnLeft className="text-sm text-secondary">
+        <p className="text-primary font-medium">Past Events</p>
+      </ColumnLeft>
+      <ColumnRight>
+        <div className="columns-2 gap-x-8">
+          {events?.records.length ? (
+            <EventsList events={events.records} />
+          ) : (
+            "No events found"
+          )}
+        </div>
+      </ColumnRight>
+    </TwoColumnLayout>
+  );
+};
+
+export default Home;

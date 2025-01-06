9cb49872f02751f9d7057fde20658e675b266009
Scott Cazan <scottcazan@gmail.com>
2025-01-06 12:45:51 +0100

    fix padding on header

--- src/components/SiteHeader/SiteHeaderDesktop.tsx
+++ src/components/SiteHeader/SiteHeaderDesktop.tsx
@@ -11,7 +11,7 @@ export const SiteHeaderDesktop = async ({
 }) => {
   return (
     <header className="hidden top-0 min-h-20 sm:flex items-center lg:gap-40 text-nav">
-      <TwoColumnLayout className="py-0 md:py-0 lg:items-center pt-7 md:pt-7 lg:pt-7">
+      <TwoColumnLayout className="py-0 md:py-7 lg:items-center lg:py-7 pt-7">
         <ColumnLeft>{logo}</ColumnLeft>
         <ColumnRight className="items-center">{nav}</ColumnRight>
       </TwoColumnLayout>

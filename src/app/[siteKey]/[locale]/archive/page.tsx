edd77f6130e4ce4785bdb9865d271e914af9f974
Scott Cazan <scottcazan@gmail.com>
2025-01-08 15:42:20 +0100

    order archive events properly

--- src/app/[siteKey]/[locale]/archive/page.tsx
+++ src/app/[siteKey]/[locale]/archive/page.tsx
@@ -20,9 +20,8 @@ const ArchivePage = async ({ params }: { params: Promise<Params> }) => {
   const { siteKey, locale } = await params;
   setConfig({ siteKey });
 
-  // const { data: events } = await getEvents({ limit: 60, dir: "asc" });
   const [{ data: events }, { data: page }, { data: site }] = await Promise.all([
-    getEvents({ limit: 60, lt: new Date().getTime() }),
+    getEvents({ limit: 60, lt: new Date().getTime(), dir: "desc" }),
     getPage({ slug: "archive" }),
     getSite(),
   ]);

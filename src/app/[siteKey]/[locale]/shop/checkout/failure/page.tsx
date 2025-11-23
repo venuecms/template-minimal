b9fcab92d5060a375bc01f055568d749d98546ca
Scott Cazan <scottcazan@gmail.com>
2025-11-23 14:22:51 +0100

    Add placeholders for checkout success

---
+++ src/app/[siteKey]/[locale]/shop/checkout/failure/page.tsx
@@ -0,0 +1,7 @@
+export default function CheckoutFailurePage() {
+  return (
+    <div>
+      <h1>Checkout failure placeholder</h1>
+    </div>
+  );
+}
---
+++ src/app/[siteKey]/[locale]/shop/checkout/success/page.tsx
@@ -0,0 +1,7 @@
+export default function CheckoutSuccessPage() {
+  return (
+    <div>
+      <h1>Checkout Success placeholder</h1>
+    </div>
+  );
+}

1f0c26dc4ecbdbf279c4546de7dde15bfcb25ca1
John Chantler <114353216+johnchantler@users.noreply.github.com>
2026-07-17 11:29:08 +0200

    ProfileLink component, added to NewsArticle (#61)

--- src/components/News/NewsArticle.tsx
+++ src/components/News/NewsArticle.tsx
@@ -6,6 +6,8 @@ import { VenueContent, VenueImage } from "@venuecms/sdk-next";
 import { format } from "date-fns";
 import { getLocale } from "next-intl/server";
 
+import { ProfileCompact } from "../ProfileCompact";
+import { ProfileLink } from "../ProfileLink";
 import { ColumnLeft, ColumnRight, TwoColumnLayout } from "../layout";
 import { renderedStyles } from "../utils";
 import { NewsArticleNav } from "./NewsArticleNav";
@@ -20,6 +22,7 @@ export const NewsArticle = async ({
   title?: string;
 }) => {
   const [locale, records] = await Promise.all([getLocale(), getNewsRecords()]);
+  const { artists = [] } = article;
   const { content } = getLocalizedContent(article?.localizedContent, locale);
 
   const index = records.findIndex((record) => record.slug === article.slug);
@@ -46,6 +49,11 @@ export const NewsArticle = async ({
           content={content}
           contentStyles={renderedStyles}
         />
+        <div className="flex flex-col gap-2">
+          {artists.map(({ profile }) => (
+            <ProfileLink key={profile.slug} profile={profile} />
+          ))}
+        </div>
         <NewsArticleNav newerSlug={newerSlug} olderSlug={olderSlug} />
       </ColumnRight>
     </TwoColumnLayout>
